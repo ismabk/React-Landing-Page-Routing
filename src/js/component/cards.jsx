9c487a86d3c691aa0ed04524d8cc4b083634793c
ismabk <ismabk94@gmail.com>
2021-11-21 12:46:43 +0000

    Maked basic structure card

---
+++ src/js/component/cards.jsx
@@ -0,0 +1,134 @@
+import React from "react";
+
+import "../../styles/cards.scss";
+
+const Cards = () => {
+	return (
+		<React.Fragment>
+			<div className="bg-second">
+				<div className="row w-75 m-auto">
+					<div className="col bg-second center-content p-3">
+						<div className="card">
+							<img
+								src="https://phantom-marca.unidadeditorial.es/b0d578821ad7d9411bac00712a0806a5/resize/1320/f/jpg/assets/multimedia/imagenes/2021/11/07/16362788730274.jpg"
+								alt="Derbi Sevillano"
+							/>
+							<div className="card-body">
+								<h5 className="card-title">Card title</h5>
+								<p className="card-text">
+									Some quick example text to build on the card
+									title and make up the bulk of the cards
+									content.
+								</p>
+								<a href="#" className="btn btn-primary">
+									Go somewhere
+								</a>
+							</div>
+						</div>
+					</div>
+					<div className="col bg-second center-content p-3">
+						<div className="card">
+							<img
+								src="https://phantom-marca.unidadeditorial.es/b0d578821ad7d9411bac00712a0806a5/resize/1320/f/jpg/assets/multimedia/imagenes/2021/11/07/16362788730274.jpg"
+								alt="Derbi Sevillano"
+							/>
+							<div className="card-body">
+								<h5 className="card-title">Card title</h5>
+								<p className="card-text">
+									Some quick example text to build on the card
+									title and make up the bulk of the cards
+									content.
+								</p>
+								<a href="#" className="btn btn-primary">
+									Go somewhere
+								</a>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div className="row w-75 m-auto">
+					<div className="col bg-second center-content p-3">
+						<div className="card">
+							<img
+								src="https://phantom-marca.unidadeditorial.es/b0d578821ad7d9411bac00712a0806a5/resize/1320/f/jpg/assets/multimedia/imagenes/2021/11/07/16362788730274.jpg"
+								alt="Derbi Sevillano"
+							/>
+							<div className="card-body">
+								<h5 className="card-title">Card title</h5>
+								<p className="card-text">
+									Some quick example text to build on the card
+									title and make up the bulk of the cards
+									content.
+								</p>
+								<a href="#" className="btn btn-primary">
+									Go somewhere
+								</a>
+							</div>
+						</div>
+					</div>
+					<div className="col bg-second center-content p-3">
+						<div className="card">
+							<img
+								src="https://phantom-marca.unidadeditorial.es/b0d578821ad7d9411bac00712a0806a5/resize/1320/f/jpg/assets/multimedia/imagenes/2021/11/07/16362788730274.jpg"
+								alt="Derbi Sevillano"
+							/>
+							<div className="card-body">
+								<h5 className="card-title">Card title</h5>
+								<p className="card-text">
+									Some quick example text to build on the card
+									title and make up the bulk of the cards
+									content.
+								</p>
+								<a href="#" className="btn btn-primary">
+									Go somewhere
+								</a>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div className="row w-75 m-auto">
+					<div className="col bg-second center-content p-3">
+						<div className="card">
+							<img
+								src="https://phantom-marca.unidadeditorial.es/b0d578821ad7d9411bac00712a0806a5/resize/1320/f/jpg/assets/multimedia/imagenes/2021/11/07/16362788730274.jpg"
+								alt="Derbi Sevillano"
+							/>
+							<div className="card-body">
+								<h5 className="card-title">Card title</h5>
+								<p className="card-text">
+									Some quick example text to build on the card
+									title and make up the bulk of the cards
+									content.
+								</p>
+								<a href="#" className="btn btn-primary">
+									Go somewhere
+								</a>
+							</div>
+						</div>
+					</div>
+					<div className="col bg-second center-content p-3">
+						<div className="card">
+							<img
+								src="https://phantom-marca.unidadeditorial.es/b0d578821ad7d9411bac00712a0806a5/resize/1320/f/jpg/assets/multimedia/imagenes/2021/11/07/16362788730274.jpg"
+								alt="Derbi Sevillano"
+							/>
+							<div className="card-body">
+								<h5 className="card-title">Card title</h5>
+								<p className="card-text">
+									Some quick example text to build on the card
+									title and make up the bulk of the cards
+									content.
+								</p>
+								<a href="#" className="btn btn-primary">
+									Go somewhere
+								</a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</React.Fragment>
+	);
+};
+
+export default Cards;
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 
 import Footer from "./footer.jsx";
+import Cards from "./cards.jsx";
 
 const Home = () => {
 	return (
 		<React.Fragment>
+			<Cards></Cards>
 			<Footer></Footer>
 		</React.Fragment>
 	);
