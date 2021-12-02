d14cde16add4a87a3855c5c2f652d73f82a210af
ismabk <ismabk94@gmail.com>
2021-12-02 03:53:24 +0100

    maked cardItem

---
+++ src/js/component/CardItem.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+import "../../styles/CardItem.scss";
+
+const CardItem = props => {
+	return (
+		<React.Fragment>
+			<div className=" col-sm-12 col-md-6 bg-second center-content p-3">
+				<div className="card">
+					<img src={props.imgCard} alt={props.altImg} />
+					<div className="card-body">
+						<a href="#" className="hover-none">
+							<h5 className="card-title">{props.titleCard}</h5>
+							<p className="card-text">{props.bodyCard}</p>
+						</a>
+					</div>
+				</div>
+			</div>
+		</React.Fragment>
+	);
+};
+
+CardItem.propTypes = {
+	imgCard: PropTypes.string,
+	altImg: PropTypes.string,
+	titleCard: PropTypes.string,
+	bodyCard: PropTypes.string
+};
+
+export default CardItem;
