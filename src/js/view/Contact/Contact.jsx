581c360cba1b3d8fdcd01cdbabe3c8b48f48d6a8
fjrf16 <fjrf1693@hotmail.com>
2021-12-10 19:58:27 +0000

    Maked the Contact page

---
+++ src/js/view/Contact/Contact.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import HeaderT from "../Header/Header.jsx";
+import Footer from "../Footer/Footer.jsx";
+import Formu from "../Contact/components/Formu.jsx";
+
+const Contact = () => {
+	return (
+		<>
+			<HeaderT></HeaderT>
+			<Formu></Formu>
+			<Footer></Footer>
+		</>
+	);
+};
+
+export default Contact;
